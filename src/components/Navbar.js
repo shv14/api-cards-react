da5d56f0731fa9533ba1ba19ff6ebff2a046160a
Shivansh Srivastav <142407244+shv14@users.noreply.github.com>
2024-03-16 13:47:12 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,14 +9,14 @@ export default function Navbar(props) {
     return (
         <nav className="navbar navbar-expand-lg text-light font-weight-bold" style={{ backgroundColor: "#030637" }} >
             <div className="container-fluid">
-                <a className="navbar-brand text-light font-weight-bolder" href="/">{props.title}</a>
+                <a className="navbar-brand text-light font-weight-bolder" href="/api-cards-react">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item text-light">
-                            <Link className={`nav-link text-light ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
+                            <Link className={`nav-link text-light ${location.pathname === "/api-cards-react" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item text-light">
                             <Link className={`nav-link text-light ${location.pathname === "/about" ? "active" : ""}`} to="/about">{props.Page1}</Link>
