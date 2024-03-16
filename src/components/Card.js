1c22e3672850e3c1a547095aeb2f5ee7a5e5b0b7
Shivansh Srivastav <142407244+shv14@users.noreply.github.com>
2024-03-16 13:45:30 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,14 +5,14 @@ export default function Card(props) {
         <div className="card my-4 product" data-bs-theme={`${props.mode}`} style={{ width: 18 + 'rem' }}>
             <img src={`${props.image_url}`} className="card-img-top" style={{
                 width: "100" + "%",
-                height: 15 + "vw",
+                height: 23 + "vw",
                 objectFit: "contain",
                 padding: 12 + "px",
             }} alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{props.name}</h5>
-                <h6 className="card-title">{props.tagline.substr(0, 20)}</h6>
-                <p className="card-text">{props.description.substr(0, 75)}</p>
+                <h6 className="card-title">Price: {props.tagline}</h6>
+                <p className="card-text">{props.description}</p>
             </div>
         </div>
 
